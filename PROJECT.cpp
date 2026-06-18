c98540b40b0afc5f7e8c2b74941f21e559ec3a55
Soban Ali <soban0x0@gmail.com>
2026-06-18 20:01:50 +0500

    Updated PROJECT.cpp

--- PROJECT.cpp
+++ PROJECT.cpp
@@ -1,148 +1,221 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
-void encryptFile(char fileName[])
-{
-    FILE *input, *output;
-    char ch;
-    int key = 10;
+#define USERNAME "system"
+#define PASSWORD "security"
+#define XOR_KEY 123
 
-    input = fopen(fileName, "r");
-
-    if (input == NULL)
-    {
-        printf("File not found!\n");
-        return;
-    }
-
-    output = fopen("encrypted.txt", "w");
-
-    while ((ch = fgetc(input)) != EOF)
-    {
-        ch = ch ^ key;
-        fputc(ch, output);
-    }
-
-    fclose(input);
-    fclose(output);
-
-    printf("File encrypted successfully!\n");
-}
-
-void decryptFile(char fileName[])
-{
-    FILE *input, *output;
-    char ch;
-    int key = 10;
-
-    input = fopen(fileName, "r");
-
-    if (input == NULL)
-    {
-        printf("File not found!\n");
-        return;
-    }
-
-    output = fopen("decrypted.txt", "w");
-    output = fopen("C:\\Users\\SobanAli\\Desktop\\encrypted.txt", "w");
-
-
-    while ((ch = fgetc(input)) != EOF)
-    {
-        ch = ch ^ key;
-        fputc(ch, output);
-    }
-
-    fclose(input);
-    fclose(output);
-
-    printf("File decrypted successfully!\n");
-}
-
-void readFile(char fileName[])
-{
-    FILE *file;
-    char ch;
-
-    file = fopen(fileName, "r");
-
-    if (file == NULL)
-    {
-        printf("File not found!\n");
-        return;
-    }
-
-    printf("\n----- File Content -----\n");
-
-    while ((ch = fgetc(file)) != EOF)
-    {
-        printf("%c", ch);
-    }
-
-    printf("\n------------------------\n");
-
-    fclose(file);
-}
+void login();
+void encryptFile();
+void decryptFile();
+void readFile();
 
 int main()
 {
-    char username[50];
-    char password[50];
-
-    char fileName[100];
     int choice;
 
-    printf("Username: ");
-    scanf("%49s", username);
+    login();
 
-    printf("Password: ");
-    scanf("%49s", password);
-
-    if (strcmp(username, "admin") == 0 &&
-        strcmp(password, "1234") == 0)
+    while (1)
     {
-        printf("\nLogin Successful!\n");
+        printf("\n==============================\n");
+        printf("1. Encrypt File\n");
+        printf("2. Decrypt File\n");
+        printf("3. Read File\n");
+        printf("4. Exit\n");
+        printf("==============================\n");
 
-        printf("\n1. Encrypt File");
-        printf("\n2. Decrypt File");
-        printf("\n3. Read File");
-        printf("\nEnter Choice: ");
+        printf("Enter Choice: ");
         scanf("%d", &choice);
 
         switch (choice)
         {
             case 1:
-
-                printf("Enter file name: ");
-                scanf("%99s", fileName);
-
-                encryptFile(fileName);
+                encryptFile();
                 break;
 
             case 2:
-
-                printf("Enter file name: ");
-                scanf("%99s", fileName);
-
-                decryptFile(fileName);
+                decryptFile();
                 break;
 
             case 3:
-
-                printf("Enter file name: ");
-                scanf("%99s", fileName);
-
-                readFile(fileName);
+                readFile();
                 break;
 
+            case 4:
+                printf("Good Bye!\n");
+                exit(0);
+
             default:
-                printf("Invalid Choice!\n");
+                printf("Invalid Num!\n");
         }
-    }
-    else
-    {
-        printf("Invalid Username or Password!\n");
     }
 
     return 0;
+}
+
+void login()
+{
+    char username[50];
+    char password[50];
+    int attempts = 0;
+
+    while (1)
+    {
+        printf("\n NOTE: After Three Wrong UN & PASS ACCOUNT SUSPENDED FOR TWO MINS.\n");
+
+        printf("\n===== FILE ENC & DEC SYSTEM =====\n");
+        printf("\n PLEASE LOGIN USERNAME & PASSWORD \n");
+    
+		printf("please enter Username: ");
+        scanf("%s", username);
+
+        printf("please enter Password: ");
+        scanf("%s", password);
+
+        if (strcmp(username, USERNAME) == 0 && strcmp(password, PASSWORD) == 0)
+        {
+            printf("\nLogin Successfull!\n");
+            return;
+        }
+
+        attempts++;
+
+        if (attempts >= 3)
+        {
+            printf("\nToo many failed attempts!\n");
+            printf("Account locked for 2mins.\n");
+
+            time_t start = time(NULL);
+
+            while (difftime(time(NULL), start) < 120)
+            {
+                //working
+            }
+
+            attempts = 0;
+
+            printf("\nAccount Unlocked.\n");
+        }
+        else
+        {
+            printf("Wrong Credentials!\n");
+            printf("Attempts Left: %d\n", 3 - attempts);
+        }
+    }
+}
+
+void encryptFile()
+{
+    char filename[1000];
+    char outputFile[1000];
+
+    FILE *fp, *enc;
+
+    printf("Enter File Name: ");
+    scanf("%s", filename);
+
+    fp = fopen(filename, "rb");
+
+    if (fp == NULL)
+    {
+        printf("File Not Found!\n");
+        return;
+    }
+
+    sprintf(outputFile, "encrypted_%s", filename);
+
+    enc = fopen(outputFile, "wb");
+
+    if (enc == NULL)
+    {
+        printf("Unable to create encrypted file.\n");
+        fclose(fp);
+        return;
+    }
+
+    char ch;
+
+    while ((ch = fgetc(fp)) != EOF)
+    {
+        fputc(ch ^ XOR_KEY, enc);
+    }
+		
+		fclose(fp);
+    fclose(enc);
+
+    printf("Encrypted File Saved As: %s\n", outputFile);
+}
+
+void decryptFile()
+{
+    char filename[1000];
+    char outputFile[1000];
+
+    FILE *fp, *dec;
+
+    printf("Enter decrypted File Name: ");
+    scanf("%s", filename);
+
+    fp = fopen(filename, "rb");
+
+    if (fp == NULL)
+    {
+        printf("File Not Found!\n");
+        return;
+    }
+
+    sprintf(outputFile, "decrypted_%s", filename);
+
+    dec = fopen(outputFile, "wb");
+
+    if (dec == NULL)
+    {
+        printf("Unable to create decrypted file.\n");
+        fclose(fp);
+        return;
+    }
+
+    char ch;
+
+    while ((ch = fgetc(fp)) != EOF)
+    {
+        fputc(ch ^ XOR_KEY, dec);
+    }
+
+    fclose(fp);
+    fclose(dec);
+
+    printf("Decrypted File Saved As: %s\n", outputFile);
+}
+
+void readFile()
+{
+    char filename[1000];
+
+    FILE *fp;
+
+    printf("Enter File Name: ");
+    scanf("%s", filename);
+
+    fp = fopen(filename, "r");
+
+    if (fp == NULL)
+    {
+        printf("File Not Found!\n");
+        return;
+    }
+    
+    printf("\n === FILE =====\n");
+    
+    char ch;
+    while((ch = fgetc(fp)) != EOF)
+	{
+		putchar(ch);
+	}
+	
+	printf("\n");
+	fclose(fp);
 }
